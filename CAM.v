7c77f1bebdea584637d7c262ba28e8b16f44e0ca
Lluis Isaac <isaac.lluis@etu.umontpellier.fr>
2020-04-09 13:51:40 +0200

    Ajout de tactique

--- CAM.v
+++ CAM.v
@@ -36,7 +36,7 @@ Inductive cam_reduction (A : Set) : (stack A) -> (code A) -> (stack A) -> (code 
 		S' = (((paire A) s t)::S) -> (cam_reduction A) (t::s::S) ((cons A)::C) S' C
 | reduc_app : forall (s t : (stack_element A)) (S S' : (stack A)) (C C1 : (code A)), 
 		S' = (((paire A) s t)::S) -> (cam_reduction A) (t::((avec_code A) C s)::S) ((app A)::C1) S' (C++C1) (*Ici faut-il aussi remplacer (C++C1) par C' et mettre quelque chose de similaire à S'*).
-  
+
 Lemma pourFst:
 	forall (s t : (stack_element nat)) (S S' : (stack nat)) (C : (code nat)),
 	(cam_reduction nat) (((paire nat) s t)::S) ((fst nat)::C) (s::S) C.
@@ -83,22 +83,22 @@ trivial.
 trivial.
 Qed.
 
-Lemma pour_swap : 
+Lemma pourSwap : 
 	forall (S : (stack nat)) (C : (code nat)) (s1 s2 : (stack_element nat)),
 	(cam_reduction nat) (s1::s2::S) ((swap nat)::C) (s2::s1::S) C.
 Proof.
 intros.
 apply reduc_swap.
-reflexivity.
+trivial.
 Qed.
 
-Lemma pour_cons : 
+Lemma pourCons : 
 	forall (S : (stack nat)) (C : (code nat)) (s1 s2 : (stack_element nat)),
 	(cam_reduction nat) (s2::s1::S) ((cons nat)::C) (((paire nat) s1 s2)::S) C.
 Proof.
 intros.
 apply reduc_cons.
-reflexivity.
+trivial.
 Qed.
 
 Lemma pourApp :
@@ -118,6 +118,19 @@ Inductive cam_reduction_ref_trans (A : Set) : (stack A) -> (code A) -> (stack A)
 | reduc_trans : forall (S S' S'' : (stack A)) (C C' C'' :(code A)),
 	(cam_reduction_ref_trans A) S C S' C' -> (cam_reduction_ref_trans A) S' C' S'' C'' -> (cam_reduction_ref_trans A) S C S'' C''.
 
+Ltac remove_simple_cam_reduc :=
+  repeat
+  match goal with
+  | |- context[cam_reduction ?A (paire ?A ?s ?t::?S) (fst ?A::?C) (?s::?S) ?C] => apply reduc_fst;trivial
+  | |- context[cam_reduction ?A (paire ?A ?s ?t::?S) (snd ?A::?C) (?t::?S) ?C] => apply reduc_snd;trivial
+  | |- context[cam_reduction ?A (?s::?S) (quote ?A ?c::?C) ((constante ?A ?c)::?S) ?C] => apply reduc_quote;trivial
+  | |- context[cam_reduction ?A (?s::?S) ((cur ?A ?C)::?C1) (avec_code ?A ?C ?s::?S) ?C1] => apply reduc_cur;trivial
+  | |- context[cam_reduction ?A (?s::?S) (push ?A::?C) (?s::?s::?S) ?C] => apply reduc_push;trivial;trivial
+  | |- context[cam_reduction ?A (?s::?t::?S) (swap ?A::?C) (?t::?s::?S) ?C] => apply reduc_swap;trivial
+  | |- context[cam_reduction ?A (?s::?t::?S) (cons ?A::?C) (paire ?A ?t ?s::?S) ?C] => apply reduc_cons;trivial
+  | |- context[cam_reduction ?A (?t::(avec_code ?A ?C ?s)::?S) (app ?A::?C1) ((paire ?A ?s ?t)::?S) (?C++?C1)] => apply reduc_app;trivial
+end.
+
 (*s.S | push;(quote 0);C -> 0.s.S | C*)
 Lemma pourAjoutZero :
 	forall (S : (stack nat)) (C : (code nat)) (s : (stack_element nat)), 
@@ -125,11 +138,8 @@ Lemma pourAjoutZero :
 Proof.
 intros.
 apply reduc_trans with (s::s::S) (((quote nat) 0)::C); apply reduc_cas_base.
-apply reduc_push.
-trivial.
-trivial.
-apply reduc_quote.
-trivial.
+remove_simple_cam_reduc.
+remove_simple_cam_reduc.
 Qed.
 
 (*s.t.S | (cur push;fst;swap;snd;swap);swap;app;C -> s.t.S | C*)
@@ -140,36 +150,29 @@ Proof.
 intros.
 apply reduc_trans with (((avec_code nat) ((push nat)::(fst nat)::(swap nat)::(snd nat)::(swap nat)::nil) s)::t::S) ((swap nat)::(app nat)::C).
 apply reduc_cas_base.
-apply reduc_cur.
-trivial.
+remove_simple_cam_reduc.
 apply reduc_trans with (t::((avec_code nat) ((push nat)::(fst nat)::(swap nat)::(snd nat)::(swap nat)::nil) s)::S) ((app nat)::C).
 apply reduc_cas_base.
-apply reduc_swap.
-trivial.
+remove_simple_cam_reduc.
 apply reduc_trans with (((paire nat) s t)::S) ((push nat)::(fst nat)::(swap nat)::(snd nat)::(swap nat)::C).
 apply reduc_cas_base.
 apply reduc_app.
 trivial.
+remove_simple_cam_reduc.
 apply reduc_trans with (((paire nat) s t)::((paire nat) s t)::S) ((fst nat)::(swap nat)::(snd nat)::(swap nat)::C).
 apply reduc_cas_base.
-apply reduc_push.
-trivial.
-trivial.
+remove_simple_cam_reduc.
 apply reduc_trans with (s::((paire nat) s t)::S) ((swap nat)::(snd nat)::(swap nat)::C).
 apply reduc_cas_base.
-apply reduc_fst.
-trivial.
+remove_simple_cam_reduc.
 apply reduc_trans with (((paire nat) s t)::s::S) ((snd nat)::(swap nat)::C).
 apply reduc_cas_base.
-apply reduc_swap.
-trivial.
+remove_simple_cam_reduc.
 apply reduc_trans with (t::s::S) ((swap nat)::C).
 apply reduc_cas_base.
-apply reduc_snd.
-trivial.
+remove_simple_cam_reduc.
 apply reduc_trans with (s::t::S) C.
 apply reduc_cas_base.
-apply reduc_swap.
-trivial.
+remove_simple_cam_reduc.
 apply reduc_ref.
 Qed.
